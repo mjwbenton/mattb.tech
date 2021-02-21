9987df01024c50a5941f65b047c55af3178d91d5
Matthew Benton <m@mattb.tech>
2021-02-21 22:35:48 +0000

    Improved layout

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,16 +1,16 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen w-screen bg-gray-100 text-transparent font-mono">
+    <div className="h-screen w-screen p-8 text-transparent font-mono">
       {/* Border box */}
-      <div className="flex justify-center items-center w-5/6 h-5/6 border-with-gradient">
+      <div className="flex min-h-full justify-center items-center border-8 border-gradient">
         {/* Centered content */}
-        <div className="clipped-background-gradient">
+        <div className="background-gradient bg-clip-text">
           <header className="w-full font-light text-7xl p-8 text-center">
             <span>mattb</span>
             <span className="text-gray-100 text-opacity-50">enton</span>
           </header>
           <main className="p-8 text-black text-lg text-opacity-20">
-            <div className="w-6/12 h-96 float-left p-8 flex justify-center items-center">
+            <div className="lg:w-6/12 lg:float-left p-8 flex justify-center items-center">
               <img
                 src="/headshot.jpg"
                 width={300}
@@ -18,12 +18,13 @@ export default function Home() {
                 className="rounded-full col-span-1"
               />
             </div>
-            <div className="w-6/12 h-96 float-right p-8 table">
-              <div className="table-cell align-middle text-lg">
-                <span className="font-semibold">Developer.</span> Recently
-                finished 10 years at Amazon and IMDb. Looking for a new gig
-                later in 2021.
-              </div>
+            <div className="lg:w-6/12 lg:float-right p-8 text-lg border-gradient border-t-2 my-16">
+              <span className="font-semibold">Developer.</span> Recently
+              finished 10 years at Amazon and IMDb. Looking for a new gig later
+              in 2021.
+            </div>
+            <div className="lg:w-6/12 lg:float-right p-8 text-lg border-gradient border-t-2">
+              <span>More to come.</span>
             </div>
           </main>
         </div>
