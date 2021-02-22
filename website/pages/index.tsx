6b75fb71bfb3eb83c706478b2845a3cff301f509
Matthew Benton <m@mattb.tech>
2021-02-22 21:05:08 +0000

    Lots more visual work

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,34 +1,84 @@
+import { GitHub, Link, Linkedin, Mail } from "react-feather";
+
 export default function Home() {
   return (
-    <div className="h-screen w-screen p-8 text-transparent font-mono">
-      {/* Border box */}
-      <div className="flex min-h-full justify-center items-center border-8 border-gradient">
-        {/* Centered content */}
-        <div className="background-gradient bg-clip-text">
-          <header className="w-full font-light text-7xl p-8 text-center">
-            <span>mattb</span>
-            <span className="text-gray-100 text-opacity-50">enton</span>
-          </header>
-          <main className="p-8 text-black text-lg text-opacity-20">
-            <div className="lg:w-6/12 lg:float-left p-8 flex justify-center items-center">
-              <img
-                src="/headshot.jpg"
-                width={300}
-                height={300}
-                className="rounded-full col-span-1"
-              />
-            </div>
-            <div className="lg:w-6/12 lg:float-right p-8 text-lg border-gradient border-t-2 my-16">
-              <span className="font-semibold">Developer.</span> Recently
-              finished 10 years at Amazon and IMDb. Looking for a new gig later
-              in 2021.
-            </div>
-            <div className="lg:w-6/12 lg:float-right p-8 text-lg border-gradient border-t-2">
-              <span>More to come.</span>
-            </div>
-          </main>
+    <>
+      {/* Wrapper Box. On large screens we set this to the size of the screen so that we can use minimum height in its child. */}
+      <div className="md:h-screen md:w-screen p-4 md:p-8 text-transparent font-mono text-lg md:text-xl">
+        {/* Border box. On larger screens it sets a min-height to make the borders go to to the edge of the screen. */}
+        <div className="flex md:min-h-full justify-center items-center border-8 border-gradient">
+          {/* Centered content. Provides the text gradient. */}
+          <div className="background-gradient bg-clip-text">
+            <header className="w-full p-8 text-center text-xl md:text-2xl">
+              <span>mattb</span>
+              <span className="text-gray-100 text-opacity-50">enton</span>
+            </header>
+            <main>
+              <ContentBox float="left">
+                <img
+                  src="/headshot.jpg"
+                  width={300}
+                  height={300}
+                  className="rounded-full m-auto"
+                />
+              </ContentBox>
+              <ContentBox float="right" border>
+                <span className="font-semibold">Developer.</span>{" "}
+                <span>
+                  Recently finished 10 years at Amazon and IMDb. Looking for a
+                  new gig later in 2021.
+                </span>
+              </ContentBox>
+              <ContentBox float="right" border>
+                <ul className="space-y-4 text-right">
+                  <li>
+                    <a href="https://github.com/mjwbenton">
+                      <Icon component={GitHub} />
+                      GitHub
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/in/mattb-tech/">
+                      <Icon component={Linkedin} />
+                      Linkedin
+                    </a>
+                  </li>
+                  <li>
+                    <a href="mailto:m@mattb.tech">
+                      <Icon component={Mail} />
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </ContentBox>
+            </main>
+          </div>
         </div>
       </div>
+    </>
+  );
+}
+
+function Icon({ component }) {
+  const Component = component;
+  return (
+    <Component
+      size={18}
+      className="inline svg-stroke-end-color mr-2 -top-0.5 relative"
+    />
+  );
+}
+
+function ContentBox({ children, float, border = false, className = "" }) {
+  return (
+    <div
+      className={`max-w-full overflow-hidden md:w-6/12 my-8 p-8
+        ${border ? "border-t-2 border-gradient" : ""}
+        ${float === "left" ? "md:float-left" : ""}
+        ${float === "right" ? "md:float-right" : ""}
+        ${className}`}
+    >
+      {children}
     </div>
   );
 }
